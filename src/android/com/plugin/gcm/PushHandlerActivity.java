6ea555c3db5127a37bdc5957b7234455d599766b
icysapphire <webber975@gmail.com>
2014-06-25 22:50:48 +0200

    Update PushHandlerActivity.java

--- src/android/com/plugin/gcm/PushHandlerActivity.java
+++ src/android/com/plugin/gcm/PushHandlerActivity.java
@@ -44,7 +44,7 @@ public class PushHandlerActivity extends Activity
 
 		if (extras != null)	{
 			Bundle originalExtras = extras.getBundle("pushBundle");
-            
+            originalExtras.putBoolean("tapped", true);
             originalExtras.putBoolean("foreground", false);
             originalExtras.putBoolean("coldstart", !isPushPluginActive);
 
